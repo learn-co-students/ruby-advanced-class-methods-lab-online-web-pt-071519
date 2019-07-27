5d5da7f93ad6dccb9f56e39be541efeb6c7974eb
corbinarnett <corbin.c.arnett@gmail.com>
2019-07-27 11:14:52 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 require 'pry'
+# https://repl.it/@corbincornelius/Advanced-Class-Properties
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -21,31 +22,63 @@ class Song
     song
   end
 
-  def self.new_by_name(song_name)
+  def self.new_by_name(song_title)
     song = self.new
-    song.name = song_name
+    song.name = song_title
     song
   end
 
-  def self.create_by_name(song_name)
+  def self.create_by_name(song_title)
     song = self.create
-    song.name = song_name
-    song.save
+    song.name = song_title
     song
   end
 
-  def self.find_by_name(song_name)
+  def self.find_by_name(song_title)
     # binding.pry
     self.all.find do |song|
-      song.name == song_name
+      song.name == song_title
     end
   end
 
-  def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
+  def self.find_or_create_by_name(song_title)
+    self.find_by_name(song_title) || self.create_by_name(song_title)
   end
 
   def self.alphabetical
-    self.all.sort_by{|s| s.name}
+    self.all.sort_by{|song| song.name}
   end
+
+  def self.new_from_filename(file)
+    #file = "Thundercat - For Love I Come.mp3"
+    # binding.pry
+    segment = file.split(" - ")
+    artist_name = segment[0]
+    title = segment[1].split(".")
+    song_title = title[0]
+
+    song = self.new
+    song.name = song_title
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(file)
+    #file = "Thundercat - For Love I Come.mp3"
+    # binding.pry
+    segment = file.split(" - ")
+    artist_name = segment[0]
+    title = segment[1].split(".")
+    song_title = title[0]
+
+    song = self.create
+    song.name = song_title
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
