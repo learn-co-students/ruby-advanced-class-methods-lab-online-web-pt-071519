f8f663d81f2f1d66598c4e2e6e103cc8fc0f2703
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-29 02:21:52 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,59 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def self.create
+    song=self.new
+    @@all<<song
+      song
+  end
+
+  def self.new_by_name (song_name)
+    new_song=self.new
+    new_song.name = song_name
+    new_song
+  end
+
+  def self.create_by_name (song_name)
+    new_song=self.new
+    new_song.name=song_name
+    @@all<<new_song
+    new_song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|search_name|search_name.name==name}
+  end
+
+  def self.find_or_create_by_name(song)
+      self.find_by_name(song) ||
+      self.create_by_name(song)
+  end
+
+  def self.alphabetical
+    new_array= self.all.sort_by {|a| a.name}
+    new_array
+  end
+
+  def self.new_from_filename (mp3_file)
+    new_song=self.new
+    new_song.artist_name=mp3_file.split(" - ")[0]
+    new_song.name=mp3_file.split(" - ")[1].split(".")[0]
+    new_song
+  end
+
+  def self.create_from_filename (mp3_file)
+    new_song=self.new
+    new_song.artist_name=mp3_file.split(" - ")[0]
+    new_song.name=mp3_file.split(" - ")[1].split(".")[0]
+    @@all<<new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 
   def self.all
     @@all
